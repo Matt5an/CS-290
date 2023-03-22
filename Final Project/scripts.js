7aed55e55cb0581a9ba1275197f19c6774123627
Matt5an <59071242+Matt5an@users.noreply.github.com>
2023-03-21 22:13:44 -0700

    Update scripts.js

--- Final Project/scripts.js
+++ Final Project/scripts.js
@@ -3,15 +3,16 @@ function calc(){
     var numOne = document.getElementById('numTwo').value;
     var operator = document.getElementById('ops').value;
 
-    if(operator == '+'){
-        document.getElementById('result').value = numOne+numTwo;    }
-    else if(operator == '-'){
+    if(operator === '+'){
+        document.getElementById('result').value = numOne+numTwo;    
+    }
+    else if(operator === '-'){
         document.getElementById('result').value = numOne-numTwo;        
     }
-    else if(operator == '*'){
+    else if(operator === '*'){
         document.getElementById('result').value = numOne*numTwo;        
     }
-    else if(operator == '/'){
+    else if(operator === '/'){
         document.getElementById('result').value = numOne/numTwo;        
     }
 }
